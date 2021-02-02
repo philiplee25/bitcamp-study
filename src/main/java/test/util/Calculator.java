d273abf8b16ac9097868d9312744f08159d8e530
philiplee25 <yplunap25@gmail.com>
2021-02-02 22:20:33 +0900

    .

---
+++ src/main/java/test/util/Calculator.java
@@ -0,0 +1,14 @@
+package test.util;
+
+public class Calculator {
+  protected int result;
+
+  public void plus(int value) {
+    this.result += value;
+  }
+
+  public int getResult() {
+    return this.result;
+  }
+
+}
